e3ee340431a6e4cd505484d97f54f341e64294f1
desup1421 <152764033+desup1421@users.noreply.github.com>
2024-04-10 01:28:19 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var audio = document.querySelector(".audio");
+var rotation = document.querySelector(".cd");
+
+$(".click-me").click(function(){
+  audio.play();
+  $(".mail-lid").addClass("rotated");
+  $(".click-me").addClass("d-none");
+  setTimeout(function(){
+    $("#mail-img").addClass("slide-up");
+    $("#mail-img").addClass("d-flex");
+  }, 50); 
+  setTimeout(function(){
+    $(".mail-section").removeClass("d-flex").addClass("d-none");
+    $(".main-section").removeClass("d-none");
+  }, 2000); 
+});
+
+$(".cd").click(function(){
+  if (audio.paused) {
+    audio.play();
+    rotation.style.animationPlayState = "running";
+  } else{
+    audio.pause();
+    rotation.style.animationPlayState = "paused";
+  }
+})
